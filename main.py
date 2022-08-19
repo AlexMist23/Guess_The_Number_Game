e808d493a901b340a1138542f4ec789c13e97fa4
AlexMist23 <aleksander.misterkiewicz@outlook.com>
2022-08-19 12:41:34 +0200

    start

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-# This is a sample Python script.
+from random import randint
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    rand_num = randint(1, 100)
+    while True:
+        guessed_number = int(input('Guess the number: '))
+        if guessed_number < rand_num:
+            print('To small!')
+        elif guessed_number > rand_num:
+            print('To big!')
+        else:
+            print('You win!')
+            break
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
