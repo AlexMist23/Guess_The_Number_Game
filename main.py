b9d64c917068df080a25a8d7798d49c360ac9159
AlexMist23 <aleksander.misterkiewicz@outlook.com>
2022-08-19 12:46:59 +0200

    1.1

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from random import randint
 
 if __name__ == '__main__':
     rand_num = randint(1, 100)
+    print('Guess the number between 1 and 100!')
     while True:
-        guessed_number = int(input('Guess the number: '))
+        try:
+            guessed_number = int(input('Guess the number: '))
+        except ValueError:
+            print('You need to enter a integer!')
+            continue
         if guessed_number < rand_num:
             print('To small!')
         elif guessed_number > rand_num:
